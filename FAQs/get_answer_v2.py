ca905f3ebdaabd4081b826f40ce19d99710f7f58
Punit-Koujalgi <41366394+Punit-Koujalgi@users.noreply.github.com>
2025-11-01 09:12:19 +0530

    faq backend generator

--- FAQs/get_answer_v2.py
+++ FAQs/get_answer_v2.py
@@ -2,7 +2,6 @@
 """
 Created on Tue Jun 22 12:32:10 2021
 
-@author: Akhil
 """
 
 # import nltk
@@ -102,4 +101,5 @@ if __name__=="__main__":
     c='''
     Super Bowl 50 was an American football game to determine the champion of the National Football League (NFL) for the 2015 season. The American Football Conference (AFC) champion Denver Broncos defeated the National Football Conference (NFC) champion Carolina Panthers 24–10 to earn their third Super Bowl title. The game was played on February 7, 2016, at Levi\'s Stadium in the San Francisco Bay Area at Santa Clara, California. As this was the 50th Super Bowl, the league emphasized the "golden anniversary" with various gold-themed initiatives, as well as temporarily suspending the tradition of naming each Super Bowl game with Roman numerals (under which the game would have been known as "Super Bowl L"), so that the logo could prominently feature the Arabic numerals 50.
     '''
+
     print(extract_answers(c))
